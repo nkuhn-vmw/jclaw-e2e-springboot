fdfc921022c3d88931d48f8bb22abdb7a094587f
vibe-coder <vibe-coder@u-desktop-t2.kuhn-labs.com>
2026-04-10 23:15:23 -0400

    feat: scaffold Spring Boot todo-api project

---
+++ src/test/java/com/example/todo/TodoApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
